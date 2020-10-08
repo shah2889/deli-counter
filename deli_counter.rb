16281713986dbb66ceb491ea3ba00a58bdd8511f
iamShah <formal>
2020-10-08 15:09:02 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+# Write your code here.
+require 'pry'
+def line(katz_deli)
+   if katz_deli==[]
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      message<< " #{index +1}. #{name}"
+    end
+    puts message
+  end
+end
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+def now_serving(katz_deli)
+ sentence="There is nobody waiting to be served!"
+ puts sentence
+ binding.pry
+end
